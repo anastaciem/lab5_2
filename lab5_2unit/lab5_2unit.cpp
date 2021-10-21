bcb1c7ef63ceb11fccf275a787b3f0cfd7fc0340
anastaciem <91271992+anastaciem@users.noreply.github.com>
2021-10-22 02:24:24 +0300

    Add files via upload

---
+++ lab5_2unit/lab5_2unit.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "D:/Університет/АП/Лабораторні/Тема 5/vs/lab5_2/lab5_2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace lab5_2unit
+{
+	TEST_CLASS(lab5_2unit)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int x = 0;
+			int n = 1;
+			int R = -(x * x / (4 * n * n - 2 * n));
+			Assert::AreEqual(R, 0);
+		}
+	};
+}
